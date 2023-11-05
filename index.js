b1b53be71bb001f6ab9c2fba6697034a2c8ca6c0
Emmanuel Ekopimo <emmanuelekopimo@gmail.com>
2023-11-05 00:51:15 -0700

    .

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Elements
 const drawArea = document.querySelector(".draw-area");
 const updatesButton = document.querySelector(".updates-button");
 const modal = document.querySelector(".modal");
@@ -11,6 +12,7 @@ const textMajor = document.querySelector(".text-major");
 const textDetails = document.querySelector(".text-details");
 const daysPanel = document.querySelectorAll("tbody>tr");
 
+// Day is not the same as Javascript day counting
 const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
 fetch("./data.json")
@@ -19,7 +21,7 @@ fetch("./data.json")
   })
   .then((data) => {
     // Structure the data
-    let customDateStr = "Oct 26 2023 9:38:00 ";
+    let customDateStr = "Nov 2 2023 9:38:00 ";
     var today = new Date();
     let id = data.id;
     let name = data.name;
@@ -37,7 +39,7 @@ fetch("./data.json")
       timeElement.style.top = "0px";
       timeElement.style.left = todayLeft.toString() + "px";
       timeElement.style.width = "4px";
-      timeElement.style.height = "9px";
+      timeElement.style.height = "10px";
       drawArea.append(timeElement);
     }
     // Add day mark
@@ -49,7 +51,8 @@ fetch("./data.json")
       }
     }
 
-    // Sort the cards according to time
+    // Sort the cards according to time they are scheduled for
+    // Forget the funny variable naming. Just to help me get it right
     cards.sort((bigCard, smallCard) => {
       let bigPower = Number(
         bigCard.day.toString() + bigCard.start[0].toString()
@@ -63,6 +66,7 @@ fetch("./data.json")
     //cards
     let hRatio = 17;
     onGoing.innerHTML = `Dear werey, no class is ongoing. Read your books. Now wey you get time 🙂`;
+    // For each card
     cards.forEach((card) => {
       let start = (card.start[0] - 6) * 60 + card.start[1];
       let end = (card.end[0] - 6) * 60 + card.end[1];
@@ -84,8 +88,7 @@ fetch("./data.json")
                     <br>
                     <span class="card-location">${card.location}</span> `;
 
-      // Now
-      let class_now = false;
+      // Checking class that is going on now
       if (card.day == today.getDay() - 1 && card.active && !holiday) {
         let start_minute = card.start[0] * 60 + card.start[1];
         let end_minute = card.end[0] * 60 + card.end[1];
@@ -95,21 +98,22 @@ fetch("./data.json")
           // That class is going on
           onGoing.innerHTML = `<b>Ongoing:</b> ${card.code} @ ${card.location}`;
 
-          class_now = true;
           element.classList.toggle("now", true);
           onGoing.classList.toggle("hide", false);
         }
       }
 
+      //Finally add the card element to the drawArea
       element.innerHTML = content;
       drawArea.append(element);
     });
 
     let nextClassNextWeek = true;
+
     for (cardIndex = 0; cardIndex < cards.length; cardIndex++) {
       let card = cards[cardIndex];
       let start_minute = card.start[0] * 60 + card.start[1];
-      now_minute = today.getHours() * 60 + today.getMinutes();
+      let now_minute = today.getHours() * 60 + today.getMinutes();
       //One day or public holiday
       if (holiday && holiday_type == "one-day") {
         if (card.day > today.getDay() - 1 && card.active) {
@@ -148,7 +152,9 @@ fetch("./data.json")
           }
           break;
         }
+        break;
       }
+
       //Holiday is more than one week or a break or strike
       else if (holiday && holiday_type == "break") {
         nextClassNextWeek = false;
@@ -166,19 +172,24 @@ fetch("./data.json")
         textDetails.innerText = cLocation;
         textMinor.innerHTML = `First lecture <b>on resumption</b>`;
         onGoing.innerHTML = `<b>Swixtt:</b><br>Enjoy your break`;
-      } else {
+      }
+      // No holiday or break 🥺😒
+      else {
         if (card.day >= today.getDay() - 1 && card.active) {
           // That is the next class
-          nextClassNextWeek = false;
-          textMajor.innerText = card.code;
-          textDetails.innerText = card.location;
-          if (card.day == today.getDay() - 1) {
+
+          if (card.day == today.getDay() - 1 && start_minute > now_minute) {
+            nextClassNextWeek = false;
+            textMajor.innerText = card.code;
+            textDetails.innerText = card.location;
             let intervalMins = start_minute - now_minute;
+            console.log(intervalMins);
             let cardDate = new Date();
             cardDate.setHours(0, 0, 0);
             cardDate.setMinutes(intervalMins);
             let nextHours = cardDate.getHours();
             let nextMinutes = cardDate.getMinutes();
+
             let minuteStr = "";
             if (nextMinutes > 0) {
               minuteStr = `${cardDate.getMinutes()} minute${
@@ -192,18 +203,27 @@ fetch("./data.json")
             } else {
               textMinor.innerHTML = `Next lecture in <b>${minuteStr}</b>`;
             }
+            break;
           } else {
-            let day = days[card.day];
+            nextClassNextWeek = false;
             if (card.day - (today.getDay() - 1) == 1) {
+              textMajor.innerText = card.code;
+              textDetails.innerText = card.location;
               textMinor.innerHTML = `First lecture <b>tomorrow</b>`;
+              break;
             } else {
+              textMajor.innerText = card.code;
+              textDetails.innerText = card.location;
+              let day = days[card.day];
               textMinor.innerHTML = `First lecture on <b>${day}</b>`;
+              break;
             }
           }
-          break;
         }
       }
     }
+
+    // If the next class is next week
     if (nextClassNextWeek) {
       let cCode = cards[0].code;
       let cLocation = cards[0].location;
